e2548c760954f5c08e098d9aa668719e8cc69bdb
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-05-26 23:38:48 +0530

    initial commit

---
+++ javascriptday2/script.js
@@ -0,0 +1,18 @@
+console.log("day 2 of javascript");
+console.log("this is conditional tutorial")
+let age = 4
+let grace=2
+age+=grace
+console.log("age",age)
+console.log(age+grace)
+console.log(age-grace)
+console.log(age*grace)
+console.log(age/grace)
+console.log(age**grace)
+console.log(age%grace)
+if(age>18){
+    console.log("you can vote ")
+}
+else {
+    console.log("you  can not vote")
+}
