108a3e01627bd760c9be4a8375d2f9f2241b2091
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-05-27 23:12:34 +0530

    initial commit

--- javascriptday2/script.js
+++ javascriptday2/script.js
@@ -1,8 +1,8 @@
 console.log("day 2 of javascript");
 console.log("this is conditional tutorial")
-let age = 4
-let grace=2
-age+=grace
+let age = 0
+let grace=0
+// age+=grace
 console.log("age",age)
 console.log(age+grace)
 console.log(age-grace)
@@ -13,6 +13,37 @@ console.log(age%grace)
 if(age>18){
     console.log("you can vote ")
 }
+else if(age==0){
+    console.log("are you kidding")
+}
 else {
     console.log("you  can not vote")
 }
+
+// // Comparison operators
+// == equal to 
+// != not equal 
+// === equal value and type
+// !== not equal value or not equal type
+
+// logical operators
+// && logical and 
+// || logical or
+// ! not
+
+
+
+//ternary operator
+let a=6
+let b=9
+let c= a>b ?(a-b):(b-a)
+console.log(c)
+/* 
+translate to :
+if(a>b){
+let c = a-b
+}
+else{
+    let c - b-a
+    }
+*/
