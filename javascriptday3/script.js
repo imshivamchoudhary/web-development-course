6d08c23a7552ec2f75824698ce437344555180fc
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-05-28 23:41:32 +0530

    initial commit

---
+++ javascriptday3/script.js
@@ -0,0 +1,33 @@
+console.log("I am tutorial on loops")
+// let a=1
+// for(let i = 0;i<100;i++){
+//     console.log(a+i)
+// }
+
+// let obj={
+//     name:"Shivam",
+//     role:"Programmer",
+//     company:"CodeVerseShivam"
+// }
+
+// for (const key in obj) {
+//     // let value=obj[key]
+//     console.log(key)
+    
+// }
+
+// for (const c of "SHivam") {
+//     console.log(c)
+// }
+
+// let i=0
+// while(i<6){
+//     console.log(i)
+//     i++
+// }
+
+let i = 3
+do{
+    console.log(i)
+    i++
+}while(i<2)
