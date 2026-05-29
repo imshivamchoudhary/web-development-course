8f9769169959544162a995a499118cafb6f8c8c3
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-05-29 23:10:34 +0530

    initial commit

---
+++ javascriptday5/script.js
@@ -0,0 +1,22 @@
+console.log("This is string tutorial")
+let a = "Shivam"
+
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+
+
+let b= "Sumit"
+console.log("his name is "+ a + " and his brother name is "+b)
+console.log(`his name is ${a} and his brother name is ${b}`)
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(a.length)
+console.log(a.slice(0,3))
+console.log(a.slice(3))
+console.log(a.replace("Shivam","shyam"))
+console.log(a.concat(b,"shyam","ravi"))
