0af64885cb3f37ccd7244e7b4a8aa9ff27bf1e25
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-06-04 23:05:16 +0530

    initial commit

---
+++ javascriptday7/index.js
@@ -0,0 +1,5 @@
+console.log("this is documant object model tutorial ")
+document.title="documant object model"
+document.body.style.backgroundColor="green"
+console.log(document.body)
+// documnet.getElementByClass("box").style.Color="red"
