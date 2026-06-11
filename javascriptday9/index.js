de7d65aea25a8a1679aaa804a14e4d3a28199525
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-06-11 23:28:57 +0530

    initial commit

---
+++ javascriptday9/index.js
@@ -0,0 +1,7 @@
+console.log("this Selecting by Ids, Classes, and More tutorial")
+
+let boxs=document.getElementsByClassName("box")
+console.log(boxs)
+boxs[2].style.backgroundColor="yellow"
+
+document.getElementById("yellow").style.backgroundColor="yellow"
