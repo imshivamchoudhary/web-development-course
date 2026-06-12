7724a197aecfd30747224682cdbdc0cea6aadf0d
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-06-12 23:37:27 +0530

    added new line of code in js

--- javascriptday9/index.js
+++ javascriptday9/index.js
@@ -5,3 +5,23 @@ console.log(boxs)
 boxs[2].style.backgroundColor="yellow"
 
 document.getElementById("yellow").style.backgroundColor="yellow"
+document.querySelector(".box").style.backgroundColor="pink"
+
+console.log(document.querySelectorAll(".box"))
+
+document.querySelectorAll(".box").forEach(e=>{
+    e.style.backgroundColor="pink"
+})
+
+let e= document.getElementsByTagName("div")
+console.log(e)
+e[5].matches("#yellow")
+console.log(e[5].matches("#yellow"))
+console.log(e[5].matches("#yell"))
+console.log(e[6].closest("#yellow"))
+console.log(e[6].closest("html"))
+
+console.log(document.querySelector(".cont").contains(e[4]))
+console.log(document.querySelector(".cont").contains(e[0]))
+console.log(document.querySelector(".cont").contains(document.querySelector("body")))
+console.log(document.querySelector("body").contains(document.querySelector(".cont")))
