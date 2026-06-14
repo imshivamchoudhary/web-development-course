2ee1f6e4943a15bf6fed61553910f5cfedb09b2f
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-06-14 23:38:59 +0530

    initial commit

---
+++ javascriptday10/script.js
@@ -0,0 +1,9 @@
+console.log(document.querySelector(".container").innerHTML)
+console.log(document.querySelector(".box").innerHTML)
+console.log(document.querySelector(".container").innerText)
+console.log(document.querySelector(".container").outerHTML)
+console.log(document.querySelector(".container").tagName)
+console.log(document.querySelector(".container").nodeName)
+console.log(document.querySelector(".container").hidden)
+// console.log(document.querySelector(".container").hidden=true)
+document.querySelector(".box").innerHTML="hey i am shivam"
