76951cb64ca4872b059be8e69cf0f9a9ae4b6473
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-06-22 23:20:38 +0530

    initial commit

--- javascriptday11/index.js
+++ javascriptday11/index.js
@@ -1,1 +1,5 @@
 console.log("This is Events, Event Bubbling, setInterval & setTimeout tutorial")
+let button = document.getElementById("btn")
+button.addEventListener("click",()=>{
+    document.querySelector(".box").innerHTML="<b> you were clicked</b>"
+})
