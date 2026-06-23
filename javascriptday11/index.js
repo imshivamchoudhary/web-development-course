1a45a149a02f86285cc946fd66eb5e7c6805e898
Shivam Choudhary <81731199+imshivamchoudhary@users.noreply.github.com>
2026-06-23 23:47:41 +0530

    add new html file

--- javascriptday11/index.js
+++ javascriptday11/index.js
@@ -1,5 +1,22 @@
 console.log("This is Events, Event Bubbling, setInterval & setTimeout tutorial")
 let button = document.getElementById("btn")
-button.addEventListener("click",()=>{
-    document.querySelector(".box").innerHTML="<b> you were clicked</b>"
+// button.addEventListener("click",()=>{
+//     document.querySelector(".box").innerHTML="<b> you were clicked</b>"
+// })
+
+
+button.addEventListener("dblclick", () => {
+    document.querySelector(".box").innerHTML = "hey you were <b>clicked</b>"
+})
+
+button.addEventListener("contextmenu", () => {
+    alert("don't right clicked please !!")
+})
+
+button.addEventListener("keydown",(e)=>{
+    console.log(e)
+})
+
+document.addEventListener("keydown",(e)=>{
+    console.log(e)
 })
